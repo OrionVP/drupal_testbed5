a4c018298237c788df55d82693776790af51a3d4
OrionVP <93703768+OrionVP@users.noreply.github.com>
2020-11-08 10:10:28 +0000

    inventory

---
+++ 界面美化/Form2_export.Designer.cs
@@ -0,0 +1,11 @@
+﻿namespace 界面美化
+{
+    partial class Form2_export
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean
