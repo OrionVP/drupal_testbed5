55b15fd67b7e9ce2075ce5ade6bd81895ba1434f
OrionVP <93703768+OrionVP@users.noreply.github.com>
2020-12-01 22:02:19 +0000

    fix stupid error

--- 界面美化/Form2_export.Designer.cs
+++ 界面美化/Form2_export.Designer.cs
@@ -157,4 +157,15 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(570, 34);
-         
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 15);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "收款日期：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(394, 119);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 15);
+            this.label8
