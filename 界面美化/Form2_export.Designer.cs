2861ae206f50d57ac595a3e7447e79a80f74abb2
OrionVP <93703768+OrionVP@users.noreply.github.com>
2020-12-17 16:17:11 +0000

    Removed extra comma

--- 界面美化/Form2_export.Designer.cs
+++ 界面美化/Form2_export.Designer.cs
@@ -272,4 +272,14 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "销售单号：";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Locati
