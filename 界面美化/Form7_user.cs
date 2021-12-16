f260313cc79a0deb7f61413c7464466d4f607dec
OrionVP <93703768+OrionVP@users.noreply.github.com>
2021-12-16 13:32:37 +0000

    updated build.gradle

--- 界面美化/Form7_user.cs
+++ 界面美化/Form7_user.cs
@@ -11,4 +11,18 @@ using System.Windows.Forms;
 
 namespace 界面美化
 {
-    public partial class Form
+    public partial class Form7_user : Form
+    {
+        public Form7_user()
+        {
+            InitializeComponent();
+        }
+        string str = "Server=LAPTOP-17RT7OKE;Database=库存管理系统;Trusted_Connection=Yes;Connect Timeout=90";
+        public  void refresh()
+        {
+            //树的生成
+            treeView1.Nodes[0].Remove();
+            TreeNode root = new TreeNode("所有用户");
+            String sql = "select 权限 from 用户种类 ";
+            DataSet dataset = new DataSet();
+            S
