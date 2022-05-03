1360b38466c2e5fc2c5d7003906cf1fa5b031ee9
OrionVP <93703768+OrionVP@users.noreply.github.com>
2022-05-03 06:47:02 +0000

    perf/x86/intel: Use proper dTLB-load-misses event on IvyBridge

--- 界面美化/Form_add.Designer.cs
+++ 界面美化/Form_add.Designer.cs
@@ -20,4 +20,15 @@
             base.Dispose(disposing);
         }
 
-        #region Windows
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 =
