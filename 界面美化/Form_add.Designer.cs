d045238a3ab284574ed289ba3aa3c5d60712bae5
OrionVP <93703768+OrionVP@users.noreply.github.com>
2022-05-30 18:29:18 +0000

    Update CV.html

--- 界面美化/Form_add.Designer.cs
+++ 界面美化/Form_add.Designer.cs
@@ -343,4 +343,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Window
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        pri
