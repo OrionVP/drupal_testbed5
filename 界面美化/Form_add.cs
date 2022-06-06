dacbfa868db17ac4bc09c991b21c175b23d0d385
OrionVP <93703768+OrionVP@users.noreply.github.com>
2022-06-06 14:13:54 +0000

    Update submodule ref

--- 界面美化/Form_add.cs
+++ 界面美化/Form_add.cs
@@ -17,4 +17,17 @@ namespace 界面美化
         {
             InitializeComponent();
         }
-        string str = "Server=LAPT
+        string str = "Server=LAPTOP-17RT7OKE;Database=库存管理系统;Trusted_Connection=Yes;Connect Timeout=90";
+        public class owner
+        {
+            public static String text=null;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("客户名称为空");
+            }
+            else {
+                if (owner.text == "客户名称：")
+        
