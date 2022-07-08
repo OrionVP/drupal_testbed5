7403c918f1d217b8f1415ab1b377650f2256aedd
OrionVP <93703768+OrionVP@users.noreply.github.com>
2022-07-08 20:04:17 +0000

    env vars

---
+++ 界面美化/Form_change.Designer.cs
@@ -0,0 +1,12 @@
+﻿namespace 界面美化
+{
+    partial class Form_change
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+  
