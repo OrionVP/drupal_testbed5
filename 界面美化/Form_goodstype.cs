0d08086ee8f92ce3df0e5a0af7e129515cf1ec4a
OrionVP <93703768+OrionVP@users.noreply.github.com>
2022-09-06 18:20:22 +0000

    remove unnecessary class

--- 界面美化/Form_goodstype.cs
+++ 界面美化/Form_goodstype.cs
@@ -11,4 +11,17 @@ using System.Windows.Forms;
 
 namespace 界面美化
 {
-    publ
+    public partial class Form_goodstype : Form
+    {
+        public Form_goodstype()
+
+        {
+            InitializeComponent();
+        }
+        string str = "Server=LAPTOP-17RT7OKE;Database=库存管理系统;Trusted_Connection=Yes;Connect Timeout=90";
+        String select = null;
+        TreeNode root = new TreeNode("所有商品");
+        public void refresh(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            for (int i =
