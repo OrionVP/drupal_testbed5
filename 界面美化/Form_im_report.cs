2da1f97eed754a657a6d230aabde712ebeabb5c5
OrionVP <93703768+OrionVP@users.noreply.github.com>
2022-10-17 08:48:12 +0000

    [CI] update locales

--- 界面美化/Form_im_report.cs
+++ 界面美化/Form_im_report.cs
@@ -28,4 +28,16 @@ namespace 界面美化
         private void Form_ex_auditing_FormClosed(object sender, FormClosedEventArgs e)
         {
             Form_menu fm = new Form_menu();
-            fm = (Form_menu)this.
+            fm = (Form_menu)this.Owner;
+            fm.Deletetabpage("采购报表");
+        }
+        public void refresh()
+        {
+            dataGridView1.DataSource = null;
+            String sql = "select * from 采购订单";
+            DataSet dataset = new DataSet();
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+            adapter.Fill(dataset, "采购订单");
+            dataGrid
