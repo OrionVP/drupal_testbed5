0fd56ba99a1e2694c7fe5855d0a0a7c9e89e7ac5
OrionVP <93703768+OrionVP@users.noreply.github.com>
2022-10-17 21:05:08 +0000

    Update CSS.md

---
+++ 界面美化/Form_menu.Designer.cs
@@ -0,0 +1,301 @@
+
+﻿namespace 界面美化
+{
+    partial class Form_menu
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_menu));
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.基本信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.客户资料ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.客户类别设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.供应商资料ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.供应商类别设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品资料ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品类别设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.出库管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.销售出库ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.入库管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.采购进货ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.库存管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品库存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.销售订单审核ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.采购订单审核ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.统计报表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.采购报表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.销售报表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.系统管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.操作员设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.窗口ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.皮肤ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.蓝色经典ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.银色金属ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.灰色怀旧ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.默认皮肤ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.styleManager1 = new DevComponents.DotNetBar.StyleManager(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton9 = new System.Windows.Forms.ToolStripSplitButton();
+            this.蓝色经典ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.灰色简约ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.银色金属ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.默认皮肤ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(0, 0);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.TabIndex = 0;
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.Location = new System.Drawing.Point(0, 0);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.基本信息ToolStripMenuItem,
+            this.出库管理ToolStripMenuItem,
+            this.入库管理ToolStripMenuItem,
+            this.库存管理ToolStripMenuItem,
+            this.统计报表ToolStripMenuItem,
+            this.系统管理ToolStripMenuItem,
+            this.窗口ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1601, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 基本信息ToolStripMenuItem
+            // 
+            this.基本信息ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.客户资料ToolStripMenuItem,
+            this.客户类别设置ToolStripMenuItem,
+            this.供应商资料ToolStripMenuItem1,
+            this.供应商类别设置ToolStripMenuItem,
+            this.商品资料ToolStripMenuItem,
+            this.商品类别设置ToolStripMenuItem});
+            this.基本信息ToolStripMenuItem.Name = "基本信息ToolStripMenuItem";
+            this.基本信息ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.基本信息ToolStripMenuItem.Text = "基本信息";
+            // 
+            // 客户资料ToolStripMenuItem
+            // 
+            this.客户资料ToolStripMenuItem.Name = "客户资料ToolStripMenuItem";
+            this.客户资料ToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.客户资料ToolStripMenuItem.Text = "客户资料";
+            this.客户资料ToolStripMenuItem.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // 客户类别设置ToolStripMenuItem
+            // 
+            this.客户类别设置ToolStripMenuItem.Name = "客户类别设置ToolStripMenuItem";
+            this.客户类别设置ToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.客户类别设置ToolStripMenuItem.Text = "客户类别设置";
+            this.客户类别设置ToolStripMenuItem.Click += new System.EventHandler(this.客户类别设置ToolStripMenuItem_Click);
+            // 
+            // 供应商资料ToolStripMenuItem1
+            // 
+            this.供应商资料ToolStripMenuItem1.Name = "供应商资料ToolStripMenuItem1";
+            this.供应商资料ToolStripMenuItem1.Size = new System.Drawing.Size(189, 26);
+            this.供应商资料ToolStripMenuItem1.Text = "供应商资料";
+            this.供应商资料ToolStripMenuItem1.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // 供应商类别设置ToolStripMenuItem
+            // 
+            this.供应商类别设置ToolStripMenuItem.Name = "供应商类别设置ToolStripMenuItem";
+            this.供应商类别设置ToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.供应商类别设置ToolStripMenuItem.Text = "供应商类别设置";
+            this.供应商类别设置ToolStripMenuItem.Click += new System.EventHandler(this.供应商类别设置ToolStripMenuItem_Click);
+            // 
+            // 商品资料ToolStripMenuItem
+            // 
+            this.商品资料ToolStripMenuItem.Name = "商品资料ToolStripMenuItem";
+            this.商品资料ToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.商品资料ToolStripMenuItem.Text = "商品资料";
+            this.商品资料ToolStripMenuItem.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // 商品类别设置ToolStripMenuItem
+            // 
+            this.商品类别设置ToolStripMenuItem.Name = "商品类别设置ToolStripMenuItem";
+            this.商品类别设置ToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.商品类别设置ToolStripMenuItem.Text = "商品类别设置";
+            this.商品类别设置ToolStripMenuItem.Click += new System.EventHandler(this.商品类别设置ToolStripMenuItem_Click);
+            // 
+            // 出库管理ToolStripMenuItem
+            // 
+            this.出库管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.销售出库ToolStripMenuItem});
+            this.出库管理ToolStripMenuItem.Name = "出库管理ToolStripMenuItem";
+            this.出库管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.出库管理ToolStripMenuItem.Text = "出库管理";
+            // 
+            // 销售出库ToolStripMenuItem
+            // 
+            this.销售出库ToolStripMenuItem.Name = "销售出库ToolStripMenuItem";
+            this.销售出库ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.销售出库ToolStripMenuItem.Text = "销售出库";
+            this.销售出库ToolStripMenuItem.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // 入库管理ToolStripMenuItem
+            // 
+            this.入库管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.采购进货ToolStripMenuItem});
+            this.入库管理ToolStripMenuItem.Name = "入库管理ToolStripMenuItem";
+            this.入库管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.入库管理ToolStripMenuItem.Text = "入库管理";
+            // 
+            // 采购进货ToolStripMenuItem
+            // 
+            this.采购进货ToolStripMenuItem.Name = "采购进货ToolStripMenuItem";
+            this.采购进货ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.采购进货ToolStripMenuItem.Text = "采购进货";
+            this.采购进货ToolStripMenuItem.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // 库存管理ToolStripMenuItem
+            // 
+            this.库存管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.商品库存ToolStripMenuItem,
+            this.销售订单审核ToolStripMenuItem,
+            this.采购订单审核ToolStripMenuItem});
+            this.库存管理ToolStripMenuItem.Name = "库存管理ToolStripMenuItem";
+            this.库存管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.库存管理ToolStripMenuItem.Text = "库存管理";
+            this.库存管理ToolStripMenuItem.Click += new System.EventHandler(this.库存管理ToolStripMenuItem_Click);
+            // 
+            // 商品库存ToolStripMenuItem
+            // 
+            this.商品库存ToolStripMenuItem.Name = "商品库存ToolStripMenuItem";
+            this.商品库存ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.商品库存ToolStripMenuItem.Text = "商品库存";
+            this.商品库存ToolStripMenuItem.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // 销售订单审核ToolStripMenuItem
+            // 
+            this.销售订单审核ToolStripMenuItem.Name = "销售订单审核ToolStripMenuItem";
+            this.销售订单审核ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.销售订单审核ToolStripMenuItem.Text = "销售订单审核";
+            this.销售订单审核ToolStripMenuItem.Click += new System.EventHandler(this.销售订单审核ToolStripMenuItem_Click);
+            // 
+            // 采购订单审核ToolStripMenuItem
+            // 
+            this.采购订单审核ToolStripMenuItem.Name = "采购订单审核ToolStripMenuItem";
+            this.采购订单审核ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.采购订单审核ToolStripMenuItem.Text = "采购订单审核";
+            this.采购订单审核ToolStripMenuItem.Click += new System.EventHandler(this.采购订单审核ToolStripMenuItem_Click);
+            // 
+            // 统计报表ToolStripMenuItem
+            // 
+            this.统计报表ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.采购报表ToolStripMenuItem,
+            this.销售报表ToolStripMenuItem});
+            this.统计报表ToolStripMenuItem.Name = "统计报表ToolStripMenuItem";
+            this.统计报表ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.统计报表ToolStripMenuItem.Text = "统计报表";
+            // 
+            // 采购报表ToolStripMenuItem
+            // 
+            this.采购报表ToolStripMenuItem.Name = "采购报表ToolStripMenuItem";
+            this.采购报表ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.采购报表ToolStripMenuItem.Text = "采购报表";
+            this.采购报表ToolStripMenuItem.Click += new System.EventHandler(this.采购报表ToolStripMenuItem_Click);
+            // 
+            // 销售报表ToolStripMenuItem
+            // 
+            this.销售报表ToolStripMenuItem.Name = "销售报表ToolStripMenuItem";
+            this.销售报表ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.销售报表ToolStripMenuItem.Text = "销售报表";
+            this.销售报表ToolStripMenuItem.Click += new System.EventHandler(this.销售报表ToolStripMenuItem_Click);
+            // 
+            // 系统管理ToolStripMenuItem
+            // 
+            this.系统管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.操作员设置ToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.系统管理ToolStripMenuItem.Name = "系统管理ToolStripMenuItem";
+            this.系统管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
