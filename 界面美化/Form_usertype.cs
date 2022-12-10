f840574dbaa5cae2d1dbf8eb03648ced11971a1e
OrionVP <93703768+OrionVP@users.noreply.github.com>
2022-12-10 05:51:21 +0000

    Version 0.0.3

--- 界面美化/Form_usertype.cs
+++ 界面美化/Form_usertype.cs
@@ -13,4 +13,16 @@ namespace 界面美化
 {
     public partial class Form_usertype : Form
     {
-  
+        public Form_usertype()
+        {
+            InitializeComponent();
+        }
+        string str = "Server=LAPTOP-17RT7OKE;Database=库存管理系统;Trusted_Connection=Yes;Connect Timeout=90";
+        String select = null;
+        TreeNode root = new TreeNode("所有用户");
+        public void refresh(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < root.Nodes.Count; i++)
+            {
+                dataGridV
