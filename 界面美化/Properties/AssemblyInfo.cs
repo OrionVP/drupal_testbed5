613819cd2c5e56b0bd7fc289204ffaa722bfea73
OrionVP <93703768+OrionVP@users.noreply.github.com>
2023-05-07 16:26:17 +0000

    Update Security.md

--- 界面美化/Properties/AssemblyInfo.cs
+++ 界面美化/Properties/AssemblyInfo.cs
@@ -20,4 +20,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[
+[assembly: Guid("dcd89144-330b-4643-a167-f46dc53bc3dd")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
+// 方法是按如下所示使用“*”: :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
